9da564aa2232975da2af6e58cf9780d93aee3c14
shandudu <2232275482@qq.com>
2024-06-10 23:26:26 +0800

    111

---
+++ playwright01/tests/my_search_baidu.py
@@ -0,0 +1,7 @@
+from playwright.sync_api import Page, expect
+
+def test_baidu(page: Page):
+    page.goto(url="https://www.baidu.com")
+    page.locator('//input[@name="wd"]').fill("playwright")
+    page.get_by_text("百度一下").click()
+    expect(page.get_by_text("https://github.com/microsoft/playwright")).to_be_visible()

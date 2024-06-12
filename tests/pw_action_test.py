7329bc28ad86f7b9c19e65bfc1bae670161ae0d2
xxx <shandudu233@gmail.com>
2024-06-12 22:40:55 +0800

    111

---
+++ tests/pw_action_test.py
@@ -0,0 +1,23 @@
+import re
+from playwright.sync_api import Page, expect
+
+
+def test_pw_action(page: Page) -> None:
+    page.goto("/demo/button")
+    page.wait_for_timeout(10000)
+    page.get_by_text("点击我试试1").click(modifiers=["Control"])
+    page.get_by_text("点击我试试1").click(button="right")
+    page.get_by_text("点击我试试1").click(position={"x": 15, "y": 20})
+    page.get_by_text("点击我试试1").click(click_count=3, delay=1_000)
+    page.get_by_text("点击我试试1").click(timeout=3_000)
+    page.get_by_text("点击我试试1").click(force=True)
+    page.get_by_text("点击我试试1").click(no_wait_after=True)
+    page.get_by_text("点击我试试1").click(trial=True)
+    page.get_by_text("点击我试试1").dblclick()
+
+
+def test_pw_notification_message(page: Page) -> None:
+    page.goto("/demo/button")
+    # page.wait_for_timeout(2_000)
+    page.get_by_text("点击我试试2").click()
+    expect(page.get_by_text("点击成功2!")).to_be_visible()

32d57780307772e5273fc257663c3d22e480cb95
shandudu <2232275482@qq.com>
2024-06-13 22:51:24 +0800

    提交input，textarea方法

--- playwright01/tests/pw_action_test.py
+++ playwright01/tests/pw_action_test.py
@@ -37,3 +37,22 @@ def test_pw_dropdown(page: Page) -> None:
     page.get_by_text("selenium").click()
     expect(page.get_by_text("你选择了webdriver")).to_be_visible()
 
+
+def test_pw_input(page: Page) -> None:
+    page.goto("/demo/input", wait_until="networkidle")
+    page.get_by_placeholder("不用管我,我是placeholder").fill("12345\n")
+    assert page.get_by_placeholder("不用管我,我是placeholder").input_value() == "12345"
+    page.get_by_label("也许你可以通过label来定位input输入框").fill("label输入")
+    assert page.get_by_label("也许你可以通过label来定位input输入框").input_value() == "label输入"
+    page.get_by_label("数字输入专用").fill("1.5682345678952130123")
+    page.get_by_label("数字输入专用").blur()
+    page.wait_for_timeout(1_000)
+    assert page.get_by_label("数字输入专用").input_value() == "1.5682345679"
+
+
+def test_pw_textarea(page: Page) -> None:
+    page.goto("/demo/textarea", wait_until="networkidle")
+    page.locator("textarea").fill("12345")
+    page.locator("textarea").fill("12345\n6")
+    page.locator("textarea").fill("""123456""")
+    expect(page.locator("textarea")).to_have_value("123\n     456")

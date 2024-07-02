9a0b0b904f095326676f6515ded931e9264ada0d
shandudu <2232275482@qq.com>
2024-07-02 22:28:31 +0800

    test_get_by_text,test_get_by_label,test_get_by_placeholder,test_get_by_title,test_get_by_alt_text,test_get_by_test_id

--- playwright01/tests/pw_locator_test.py
+++ playwright01/tests/pw_locator_test.py
@@ -1,10 +1,8 @@
-
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Page, expect, Playwright
 
 
 def test_get_by_role(page: Page):
     """
-
     :param page:
     :return:
     """
@@ -12,8 +10,8 @@ def test_get_by_role(page: Page):
     page.get_by_text(text="点我开启一个dialog").click()
     expect(page.get_by_role(role="dialog")).to_be_visible()
     page.goto("/demo/checkbox", wait_until="networkidle")
-    page.get_by_role(role="checkbox", name="开发",checked=False).set_checked(True)
-    page.get_by_role(role="checkbox", name="开发",checked=True).set_checked(False)
+    page.get_by_role(role="checkbox", name="开发", checked=False).set_checked(True)
+    page.get_by_role(role="checkbox", name="开发", checked=True).set_checked(False)
     page.goto("/demo/table", wait_until="networkidle")
     expect(page.get_by_role(role="table")).to_be_visible()
     expect(page.get_by_role(role="cell")).to_have_count(count=13)
@@ -21,3 +19,37 @@ def test_get_by_role(page: Page):
     page.goto("/demo/grid", wait_until="networkidle")
     expect(page.get_by_role("treegrid")).to_be_visible()
     expect(page.get_by_role("row").filter(has_text="溜达王").locator("div").nth(1)).to_have_text("44")
+
+
+def test_get_by_text(page: Page):
+    page.goto("/demo/getbytext", wait_until="networkidle")
+    expect(page.get_by_text(text="确定")).to_have_count(3)
+    expect(page.get_by_text("确定", exact=True)).to_have_count(2)
+    expect(page.get_by_text("确定 确认 肯定")).to_have_count(1)
+
+
+def test_get_by_label(page: Page):
+    page.goto("/demo/input", wait_until="networkidle")
+    page.get_by_label("输入你想输入的任何文字").fill("1234567")
+
+
+def test_get_by_placeholder(page: Page):
+    page.goto("/demo/input", wait_until="networkidle")
+    page.get_by_placeholder("超过10位小数会被截取").fill("12345627")
+
+
+def test_get_by_title(page: Page):
+    page.goto("/demo/image", wait_until="networkidle")
+    expect(page.get_by_title("这是一个title")).to_be_visible()
+
+
+def test_get_by_alt_text(page: Page):
+    page.goto("/demo/image", wait_until="networkidle")
+    expect(page.get_by_alt_text("这是图片占位符")).to_be_visible()
+    expect(page.get_by_alt_text("图片占位符")).to_be_visible()
+
+
+def test_get_by_test_id(page: Page, playwright: Playwright):
+    page.goto("/demo/image", wait_until="networkidle")
+    playwright.selectors.set_test_id_attribute("my_test_id")
+    expect(page.get_by_test_id("Howls Moving Castle")).to_be_visible()

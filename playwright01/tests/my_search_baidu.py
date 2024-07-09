f63cbec3fd58b5bf126f1c5666a4b78f1cfa0df4
xxx <shandudu233@gmail.com>
2024-07-09 18:06:13 +0800

    提交

--- playwright01/tests/my_search_baidu.py
+++ playwright01/tests/my_search_baidu.py
@@ -1,8 +1,27 @@
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Page, expect, sync_playwright
 
-def test_baidu(page: Page):
+def pw1_baidu():
+    # pw = sync_playwright().start()
+    with sync_playwright() as pw:
+        browser = pw.chromium.launch(headless=False)
+        context = browser.new_context()
+        page = context.new_page()
+        page.goto(url="https://www.baidu.com")
+        page.locator('//input[@name="wd"]').fill("playwright")
+        page.get_by_text("百度一下").click()
+        expect(page.get_by_text("https://github.com/microsoft/playwright")).to_be_visible()
+       # pw.stop()
+
+def pw2_baidu():
+    pw = sync_playwright().start()
+    browser = pw.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
     page.goto(url="https://www.baidu.com")
     page.locator('//input[@name="wd"]').fill("playwright")
     page.get_by_text("百度一下").click()
     expect(page.get_by_text("https://github.com/microsoft/playwright")).to_be_visible()
+    pw.stop()
 
+pw1_baidu()
+pw2_baidu()
